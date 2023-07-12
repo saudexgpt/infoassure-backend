20bd3cec3664ba5c17d2b4b1bee1a51967b7f93c
sammy4best <saudexgpt@gmail.com>
2023-07-12 09:43:07 +0100

    Cors solved

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -68,6 +68,6 @@ class Kernel extends HttpKernel
         'role' => \Laratrust\Middleware\LaratrustRole::class,
         'permission' => \Laratrust\Middleware\LaratrustPermission::class,
         'ability' => \Laratrust\Middleware\LaratrustAbility::class,
-        // 'cors' => \App\Http\Middleware\Cors::class,
+        'cors' => \App\Http\Middleware\Cors::class,
     ];
 }
