252e228138cd2d897906b7d0c3fa6b120157cde0
Samuel Udoh <saudexgpt@gmail.com>
2025-01-21 22:37:16 +0100

    update

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -128,7 +128,7 @@ class AuthController extends Controller
      * @param  [string] password
      * @param  [boolean] remember_me
      */
-    public function login(Request $request)
+    public function loginNo2FA(Request $request)
     {
         $this->username = $this->findUsername();
 
@@ -154,7 +154,7 @@ class AuthController extends Controller
         // }
         return $this->generateAuthorizationKey($user);
     }
-    public function login2FA(Request $request)
+    public function login(Request $request)
     {
         $this->username = $this->findUsername();
 
