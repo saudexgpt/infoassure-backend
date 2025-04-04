762cf2ad2a57c1897f72793a9067f3c9785aa034
Samuel Udoh <saudexgpt@gmail.com>
2025-04-04 12:41:53 +0100

    test

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -354,6 +354,7 @@ class AuthController extends Controller
     }
     public function recoverPassword(Request $request)
     {
+        echo env('RECAPTCHA_SITE_SECRET');
         $request->validate([
             'email' => 'required',
             'recaptcha' => ['required', new ReCaptcha]
